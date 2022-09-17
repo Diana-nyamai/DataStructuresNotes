3bb15b32e11ab103cddb91ff76821cb03814d538
Diana-nyamai <dnyamai.dn@gmail.com>
2022-09-17 13:55:41 +0300

    insertion solution

--- insertion.js
+++ insertion.js
@@ -1,30 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>insertion</title>
-</head>
-<body>
-    <script>
-        function insertion(arr){
-            for(let i=1; i<arr.length; i++){
-                // this is index 1
-                let insertnumber = arr[i]
-                // this is index 0
-                let j = arr[i-1]
-                // while number in index zero is greater or equal to zero and it is greater than index 1 then it will compile that
-                while(j>=0 && arr[j] > insertnumber){
-                    // number in index 1 will be swapped to become index 0
-                    arr[j+1] = j
-                    // number in index 0 will be swapped to become undex 1
-                    j = j+1
-                }
-            }
-
+function insertion(arr){
+    for(let i=1; i<arr.length; i++){
+        // this is index 1
+        let insertnumber = arr[i]
+        // this is index 0
+        let j = arr[i-1]
+        // while number in index zero is greater or equal to zero and it is greater than index 1 then it will compile that
+        while(j>=0 && arr[j] > insertnumber){
+            // number in index 1 will be swapped to become index 0
+            arr[j+1] = j
+            // number in index 0 will be swapped to become undex 1
+            j = j+1
         }
-        var arr = [-3,-5,10,7]
-    </script>
-</body>
-</html>
+    }
+
+}
+var arr = [-3,-5,10,7]
