f86de69c3c9111288084398203bbcdc14d7ac763
Diana-nyamai <dnyamai.dn@gmail.com>
2022-09-17 14:00:15 +0300

    recursionfactorial solution

--- recursionfactorial.js
+++ recursionfactorial.js
@@ -1,23 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recursive factorial</title>
-</head>
-<body>
-    <script>
-        function Recursivefactorial(n){
-            if(n===0){
-                return 1
-            }
-            return n * Recursivefactorial(n -1)
-        }
-        document.write(Recursivefactorial(4) + "<br>")
-        document.write(Recursivefactorial(5) + "<br>")
+function Recursivefactorial(n){
+    if(n===0){
+        return 1
+    }
+    return n * Recursivefactorial(n -1)
+}
+console.log(Recursivefactorial(4) + "\n")
+console.log(Recursivefactorial(5) + "\n")
 
 // time complexity is O(n) - linear complexity since as the number of n increases so does the nmber of instructions to be executed increase
-    </script>
-</body>
-</html>
